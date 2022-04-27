4288d9bbf14eddc9d17cda8c212dfa759b80e0ce
Magda <magda.pacan139@gmail.com>
2022-04-27 15:00:37 +0200

    classes added

---
+++ src/com/company/MyDB.java
@@ -0,0 +1,91 @@
+package com.company;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class MyDB {
+    Connection conn = null;
+
+    MyDB() {
+        if (conn == null) open();
+    }
+
+    public void open() {
+        try {
+            String url = "jdbc:sqlite:database\\database.db";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            if (conn != null) close();
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("cannot close");
+        }
+        conn = null;
+    }
+
+    public void cmd(String sql) {
+        if (conn == null) open();
+        if (conn == null) {
+            System.out.println("No connection");
+            return;
+        }
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException E) {
+            System.out.println("Error in statement " + sql);
+        }
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in statement" + sql);
+        }
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in statement " + sql);
+        }
+    }
+
+    public ArrayList<String> query(String query, String fld) {
+        ArrayList<String> res = new ArrayList<>();
+        if (conn == null) open();
+        if (conn == null) {
+            System.out.println("No connection");
+            return res;
+        }
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String name = rs.getString(fld);
+                res.add(name);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in statement " + query + " " + fld);
+        }
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in statement " + query + " " + fld);
+        }
+        return res;
+    }
+
+}
