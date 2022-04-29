e316a0f7f1267c7dceeeba4ac854d471d3f4af20
Magda <magda.pacan139@gmail.com>
2022-04-29 20:26:38 +0200

    clean code

--- src/com/company/Model.java
+++ src/com/company/Model.java
@@ -3,31 +3,10 @@ package com.company;
 import java.util.ArrayList;
 
 public class Model {
-    Model(){
-    }
+    MyDB db = new MyDB();
 
-        MyDB db = new MyDB();
-
-    /*void addSchedule(String courseName, String time, String roomName, String teacherName){
-        db.cmd("insert into Schedule (CourseName, Time, RoomName, TeacherName) values ('%1$s, %2$s, %3$s, %4$s');", ("courseName, time, roomName, teacherName");}
-    }*/
-
-    /*void addSchedule(String courseName, String time, String roomName, String teacherName){
-        db.cmd(String.format("insert into Schedule (CourseName, Time, RoomName, TeacherName) values ('%1$s, %2$s, %3$s, %4$s');", courseName, time, roomName, teacherName));
-     void addTimeslot(String s){ // remember to sanitize your data!
-        db.cmd("insert into Timeslot (name) values ('"+s+"');");
-    }*/
-    void addSchedule (String CourseName, String Time, String RoomName, String TeacherName){
-        db.cmd("insert into Schedule (ScheduleID, CourseName, Time, RoomName, TeacherName) values (NULL, '"+CourseName+"','"+Time+"','"+RoomName+"','"+TeacherName+"');");
-    }
-
-    ArrayList<String> getSchedule() {
-        return db.query("select CourseName, Time, RoomName, TeacherName from Schedule;", "TeacherName");
-    }
-
-        ArrayList<String> getTeacher() {
-            return db.query("select TeacherName from Teacher;", "TeacherName");
-        }
+    ArrayList<String> getTeacher() {
+            return db.query("select TeacherName from Teacher;", "TeacherName");}
 
         ArrayList<String> getCourse() {
             return db.query("select CourseName from Course;", "CourseName");
@@ -41,38 +20,21 @@ public class Model {
             return db.query("select Time from TimeBlock;", "Time");
         }
 
-
-
-       void add(String s) { // remember to sanitize your data!
-            db.cmd("insert into Schedule (CourseName,Time, RoomName, TeacherName) values ('" + s + "');");
-        }
-
         ArrayList<String> get() {
-            return db.query("select CourseName,Time, RoomName, TeacherName from Schedule order by ScheduleID;", "CourseName,Time, RoomName, TeacherName");
-        }
+            return db.query("select fld1 from lst order by fld2;", "fld1");}
 
         String findRoom(String c){
-            ArrayList<String> Schedule= db.query(
+            ArrayList<String> lst= db.query(
                     "select Room.RoomName from Room inner join Course"
                             +" where Course.CourseName = '"+c+"' and Room.CapacityMax > Course.ExpectedStudents;","RoomName");
-            System.out.println(Schedule);
-            if(Schedule.size()==0)return "";
-            else return Schedule.get(0);
-        }
-
-        /*String availableTeachers(String d){
-        ArrayList<String> Schedule = db.query("select Schedule.TeacherName from Schedule inner join Schedule"
-                + "where Schedule.Time = '"+ d +"' and Schedule.Teacher != Schedule.Time;","Teacher");
-            System.out.println(Schedule);
-            if(Schedule.size()==0)return "";
-            else return Schedule.get(0);
-        }*/
-        String availableTeachers(String d) {
-            ArrayList<String> lst = db.query("select TeacherName from Schedule where TeacherName = '" + d + "' and Schedule.TeacherName != Schedule.Time;", "TeacherName");
             System.out.println(lst);
             if(lst.size()==0)return "";
-            else return lst.get(0);
+            else return String.valueOf(lst);
         }
+    void addSchedule (String CourseName, String Time, String RoomName, String TeacherName){
+        db.cmd("insert into Schedule (ScheduleID, CourseName, Time, RoomName, TeacherName) values (NULL, '"+CourseName+"','"+Time+"','"+RoomName+"','"+TeacherName+"');");}
 
-    }
+}
+
+
 
--- src/com/company/MyDB.java
+++ src/com/company/MyDB.java
@@ -88,5 +88,4 @@ public class MyDB {
         }
         return res;
     }
-
 }
